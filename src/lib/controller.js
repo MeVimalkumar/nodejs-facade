b75d948fe0e56b9b182e01590e5186d4cca22f75
Vimalkumar <mr.jainvimal@gmail.com>
2020-06-11 00:43:15 +0530

    Generic classes for facade

---
+++ src/lib/controller.js
@@ -0,0 +1,47 @@
+class Controller {
+    constructor(Model) {
+        this.Model = Model;
+    }
+
+    create(req, res, next) {
+        this.Model.create(req.body).then(doc => {
+            res.status(201).send(doc);
+        }).catch(error => {
+            next(error);
+        });
+    }
+
+    find(req, res, next) {
+        this.Model.find(req.query).then(doc => {
+            res.status(200).send(doc);
+        }).catch(error => {
+            next(error);
+        });
+    }
+
+    findById(req, res, next) {
+        this.Model.findById(req.params.id).then(doc => {
+            res.status(200).send(doc);
+        }).catch(error => {
+            next(error);
+        });
+    }
+
+    findByIdAndUpdate(req, res, next) {
+        this.Model.findByIdAndUpdate(req.params.id).then(doc => {
+            res.status(204).send(doc);
+        }).catch(error => {
+            next(error);
+        });
+    }
+
+    findByIdAndDelete(req, res, next) {
+        this.Model.findByIdAndDelete(req.params.id).then(doc => {
+            res.status(204).send(doc);
+        }).catch(error => {
+            next(error);
+        });
+    }
+}
+
+module.exports = Controller
---
+++ src/lib/model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+class Model {
+    constructor(name, schema) {
+        this.model = mongoose.model(name, schema);
+    }
+
+    create(body) {
+        const doc = new this.model(body);
+        return doc.save();
+    }
+
+    find(...args) {
+        return this.model.find(...args).exec();
+    }
+
+    findById(...args) {
+        return this.model.findById(...args).exec();
+    }
+
+    findByIdAndUpdate(...args) {
+        return this.model.findByIdAndUpdate(...args).exec
+    }
+
+    findByIdAndDelete(...args) {
+        return this.model.findByIdAndDelete(...args).exec
+    }
+}
+
+module.exports = Model
